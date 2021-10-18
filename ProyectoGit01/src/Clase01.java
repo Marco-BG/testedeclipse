a845cbf0bcdc4fa2ece4b4139774a090412a9a87
Marco Benegas <180102@iessierradeguara.com>
2021-10-18 11:01:46 +0200

    Añadido hola mundo

--- ProyectoGit01/src/Clase01.java
+++ ProyectoGit01/src/Clase01.java
@@ -1,4 +1,7 @@
+import java.io.IOException;
 
 public class Clase01 {
-
+	public static void main(String[] args) throws IOException, InterruptedException {
+		System.out.println("Hola mundo");
+	}
 }
